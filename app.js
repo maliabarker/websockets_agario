36e86794d68902272c50467e725a3099f0625cdb
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-06-30 13:54:55 -1000

    added circles to array when new user connects and removes circle from array when user disconnects

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const path = require('path');
-const { builtinModules } = require('module');
+
+allCircles = []
+
 
 const HTML_DIR = path.join(__dirname, '/static/')
 app.use(express.static(HTML_DIR))
@@ -15,17 +17,61 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    // console.log('a user connected');
-    socket.emit('new connection', 'user')
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+    // MVP:
+    // create new circle on connection
+    // append circle to all circles list
+    // load in all circles
+    // allow all circles to move
+    // delete circle from list on disconnect
+
+    // EXTRA:
+    // allow users to create username
+    // add food to grow circle
+    // allow for collision detection
+    // have a restart button or function
+
+    // TODO: create new circle for each user on connection 
+    // *note: make sure user can control only their circle on connection
+
+    console.log(`${socket.id} connected`);
+
+    // emits connection message to all users
+    io.sockets.emit('connect message', socket.id);
+
+    socket.on('new circle', (circleData) => {
+        circleData.socketId = socket.id;
+        allCircles.push(circleData);
+
+        console.log(circleData);
+        console.log(allCircles);
+
+        // socket.broadcast.emit('place-me-at-all-clients-screen', data);
+        // socket.emit('place-all-clients-to-my-screen', onlineCircles);
+        // socket.emit('place-all-foods', foods);
     });
 
-    socket.on('new connection', (msg) => {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
+    // only emits to single user's server
+    // socket.emit('new circle')
+
+
+    // remove circle from array on disconnect
+    socket.on('disconnect', function() {
+        console.log(`${socket.id} disconnected`);
+        allCircles.forEach(element => {
+            if (element.socketId == socket.id) {
+                const index = allCircles.indexOf(element);
+                allCircles.splice(index, 1);
+                console.log(`${socket.id} circle removed!`)
+            };
+        });
     });
+
+    // for emitting connection msg to all users
+    // socket.on('new connection', (msg) => {
+    //     console.log('message: ' + msg);
+    //     io.emit('new connection', msg);
+    // });
 });
 
 server.listen(3000, () => {
--- static/client.js
+++ static/client.js
@@ -1,22 +1,64 @@
-// script for moving circle with mouse
-let circle = document.getElementById('circle');
-const onMouseMove = (e) => {
-    circle.style.left = e.pageX + 'px';
-    circle.style.top = e.pageY + 'px';
-}
-document.addEventListener('mousemove', onMouseMove);
+// creating a circle object
+class Circle {
+    // add all attributes
+    constructor(socketId) {
+        this._id = socketId;
+        this.x = Math.floor(Math.random() * window.screen.width);
+        this.y = Math.floor(Math.random() * window.screen.height);
+        this.hue = Math.floor(Math.random() * 360);
+        this.size = 20;
+    }
 
+    // // method to display circle
+    // display() {
+
+    // }
+
+    // // method to move circle
+    // move() {
+
+    // }
+};
+
+// ——————————————————————————————————————————————————————
 
 // websocket script
 var socket = io();
 
-socket.on('new connection', function(msg) {
-    console.log('SOMETHING')
+// will only emit to current user
+socket.on('connect message', function(msg) {
+    console.log(`${msg} connected`);
     document.getElementById("connect-msg").innerHTML = msg + ' just connected';
 
+    // TODO: make sure connection message does not move page down (move circles)
     setTimeout(function(){
         document.getElementById("connect-msg").innerHTML = '';
     }, 3000);
-
 });
+
+// Creating a new circle and emitting to server
+var circle = new Circle();
+console.log(circle);
+socket.emit('new circle', circle);
+
+// // display all circles 
+// socket.on('draw circles', function(circles) {
+//     circles.array.forEach(element => {
+//         console.log(element)
+//     });
+// })
+
+// socket.on('new circle', function() {
+//     var circleContainer = document.getElementById("circle-container");
+//     var circle = document.createElement('div');
+//     circle.setAttribute('id', 'user-circle');
+//     circleContainer.appendChild(circle);
+//     const onMouseMove = (e) => {
+//         circle.style.left = e.pageX + 'px';
+//         circle.style.top = e.pageY + 'px';
+//     }
+//     document.addEventListener('mousemove', onMouseMove);
+// });
+
+// delete circle on disconnect
 
